2ddc0379e97e4d524af5aa4c27c7ff425dd9b522
Quan-p <quanp155@gmail.com>
2021-08-27 18:35:14 -0500

    Set up default requirements

--- script.js
+++ script.js
@@ -1,37 +1,21 @@
 const email = document.getElementById('email');
-const emailError = document.querySelector('#email + span.error');
-
-function validEmail() {
-    if (email.validity.valueMissing) {
-        emailError.textContent = 'You need to enter an e-mail address';
-    } else if (email.validity.typeMismatch) {
-        emailError.textContent = 'Entered value needs to be an e-mail address';
-    }
-}
-
-email.addEventListener('input', (event) => {
-    if (email.validity.valid) {
-        emailError.textContent = '';
-        emailError.className = 'error';
-    } else {
-        validEmail();
-    }
-});
-
 const country = document.getElementById('country');
-const countryError = document.querySelector('#country + span.error');
+const zipCode = document.getElementById('zip');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirm-password');
 
-function validCountry() {
-    if (email.validity.valueMissing) {
-        emailError.textContent = 'You need to enter a country';
-    }
-}
+const form = document.getElementById('myForm');
 
-country.addEventListener('input', (event) => {
-    if (email.validity.valid) {
-        countryError.textContent = '';
-        countryError.className = 'error';
-    } else {
-        validCountry();
-    }
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
 });
+
+const isRequired = (value) => (value !== '');
+
+const lengthReq = (length, min, max) => (!(length < min || length > max));
+
+if (lengthReq) {
+    console.log('true');
+} else {
+    console.log('false');
+}
