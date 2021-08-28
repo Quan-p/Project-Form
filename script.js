bc6210b923bb0917857e8522005bd382ab7547f1
Quan-p <quanp155@gmail.com>
2021-08-28 14:13:29 -0500

    Added zipcode validation

--- script.js
+++ script.js
@@ -15,12 +15,17 @@ const isRequired = (value) => (value !== '');
 const lengthReq = (length, min, max) => (!(length < min || length > max));
 
 function emailValid(email) {
-    const check = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return check.test(email);
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+function zipValid(zipCode) {
+    const re = new RegExp('^\\d+$');
+    return re.test(zipCode);
 }
 
 function passwordValid(password) {
-    const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+    const re = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})');
     return re.test(password);
 }
 
