77040faed64e86333b0b14b5b790d47a181b1800
Quan-p <quanp155@gmail.com>
2021-08-28 13:46:28 -0500

    Set up checks for email and password. Function to display appropriate error message

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ form.addEventListener('submit', (event) => {
 const isRequired = (value) => (value !== '');
 
 const lengthReq = (length, min, max) => (!(length < min || length > max));
+
+const emailValid = (email) => {
+    const check = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return check.test(email);
+};
+
+const passwordValid = (password) => {
+    const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+    return re.test(password);
+};
+
+function showError(input, message) {
+    const formField = input.parentElement;
+
+    formField.classList.remove('success');
+    formField.classList.add('error');
+
+    const error = formField.querySelector('span');
+    error.textContent = message;
+}
